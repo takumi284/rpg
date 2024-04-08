96f925b62934407292e13d6d5c82082910a66339
Takumi Hirabayashi <221205139@ccmailg.meijo-u.ac.jp>
2024-04-08 16:41:25 +0900

    攻撃に関する処理方法の改変

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -3,7 +3,61 @@ import './Game.css';
 import playerA from '../images/logo192.png';
 import playerB from '../images/arrer.png';
 
+// function manageTurn(turn, setTurn, firstTurn, setFirstTurn) {
+//     if (turn === true && firstTurn === true) {
+//         setTurn(!turn);
+//     } else if (turn === false && firstTurn === false) {
+//         setTurn(!turn);
+//     } else if (turn === false && firstTurn === true) {
+//         setFirstTurn(!firstTurn);
+//     } else if (turn === true && firstTurn === false) {
+//         setFirstTurn(!firstTurn);
+//     }
+// }
+
+
+function manageTurn(step, turn, setStep, setTurn) {
+    setTurn(!turn);
+}
+
+// function attack(attackerName, attackerPosition, enemyPosition, attackEreaX, attackEreaY, setWinner, enemyHp, setEnemyHp, turn, setTurn, damage = 1) {
+//     if ((attackerPosition[0] - enemyPosition[0] >= -attackEreaX && attackerPosition[0] - enemyPosition[0] <= attackEreaX)
+//         && attackerPosition[1]-enemyPosition[1] <= attackEreaY
+//         && attackerPosition[1]-enemyPosition[1] >= -attackEreaY) {
+        
+//         if (attackerName === 'A' && turn === true) {
+//             setEnemyHp(enemyHp - damage);
+//             setTurn(!turn);
+//         } else if (attackerName === 'B' && turn === false) {
+//             setEnemyHp(enemyHp - damage);
+//             setTurn(!turn);
+//         }
+//     }
+// }
+
+function attack(turn, setTurn, setAttackErea, attackerName, attackEreaX, attackEreaY, damage = 1) {
+    if (turn === true && attackerName === 'A') {
+        setAttackErea([attackEreaX, attackEreaY, damage]);
+        setTurn(!turn);
+    } else if (turn === false && attackerName === 'B') {
+        setAttackErea([attackEreaX, attackEreaY, damage]);
+        setTurn(!turn);
+    }
+}
+
+function inAttackErea(myPosition, attackerPosition, attackErea) {
+    if (myPosition[0] <= attackerPosition[0] + attackErea[0] && myPosition[0] >= attackerPosition[0] - attackErea[0]
+        && myPosition[1] <= attackerPosition[1] + attackErea[1] && myPosition[1] >= attackerPosition[1] - attackErea[1]) {
+            return true;
+    }
+    return false;
+}
+
 const Player = (props) => {
+    if (props.attackedDamage > 0) {
+        props.setHp(props.hp - props.attackedDamage);
+        props.setAttackErea([0, 0, 0]);
+    }
     return (
         <div className="Player">
             <img src={props.playerType} alt='playerImg' className='playerImage'/>
@@ -12,15 +66,35 @@ const Player = (props) => {
 }
 
 const Square = (props) => {
+    let stl = null;
+    let attackDamageA = 0;
+    let attackDamageB = 0;
+    if (inAttackErea([props.x, props.y], props.positionA, props.attackEreaA)) {
+        attackDamageA = props.attackEreaA[2];
+        stl = {backgroundColor: 'red'};
+    }
+
+    if (inAttackErea([props.x, props.y], props.positionB, props.attackEreaB)) {
+        attackDamageB = props.attackEreaB[2];
+        stl = {backgroundColor: 'blue'};
+    }
+
     let player = null;
     if (props.x === props.positionA[0] && props.y === props.positionA[1]) {
-        player = <Player playerType={playerA}/>;
+
+        player = <Player playerType={playerA} myPosition={props.positionA}
+                    attackedDamage={attackDamageB} setAttackErea={props.setAttackEreaB}
+                    hp={props.hpA} setHp={props.setHpA}/>;
+
     } else if (props.x === props.positionB[0] && props.y === props.positionB[1]) {
-        player = <Player playerType={playerB}/>;
+
+        player = <Player playerType={playerB} myPosition={props.positionB}
+                    attackedDamage={attackDamageA} setAttackErea={props.setAttackEreaA}
+                    hp={props.hpB} setHp={props.setHpB}/>;
     }
 
     return (
-        <div className="Square">
+        <div className="Square" style={stl}>
             {player}
         </div> 
     );
@@ -30,7 +104,10 @@ const SquareRow = (props) => {
     const squares = Array(props.boardSize[0]).fill(0);
 
     const row = squares.map((square, x) => {
-        return <Square x={x} y={props.y} positionA={props.positionA} positionB={props.positionB}/>;
+        return <Square x={x} y={props.y} positionA={props.positionA} positionB={props.positionB}
+        attackEreaA={props.attackEreaA} attackEreaB={props.attackEreaB}
+        setAttackEreaA={props.setAttackEreaA} setAttackEreaB={props.setAttackEreaB}
+        hpA={props.hpA} setHpA={props.setHpA} hpB={props.hpB} setHpB={props.setHpB}/>;
     });
 
     return (
@@ -44,7 +121,10 @@ const Board = (props) => {
     const rows = Array(props.boardSize[1]).fill(0);
 
     let board = rows.map((row, y) => {
-        return <SquareRow y={y} positionA={props.positionA} positionB={props.positionB} boardSize={props.boardSize}/>;
+        return <SquareRow y={y} positionA={props.positionA} positionB={props.positionB} boardSize={props.boardSize}
+        attackEreaA={props.attackEreaA} attackEreaB={props.attackEreaB}
+        setAttackEreaA={props.setAttackEreaA} setAttackEreaB={props.setAttackEreaB}
+        hpA={props.hpA} setHpA={props.setHpA} hpB={props.hpB} setHpB={props.setHpB}/>;
     });
 
     return (
@@ -54,35 +134,17 @@ const Board = (props) => {
     );
 }
 
-function manageTurn(step, turn, setStep, setTurn) {
-    setTurn(!turn);
-}
-
-function attack(attackerName, attackerPosition, enemyPosition, attackEreaX, attackEreaY, setWinner, enemyHp, setEnemyHp, turn, setTurn, damage = 1) {
-    if ((attackerPosition[0] - enemyPosition[0] >= -attackEreaX && attackerPosition[0] - enemyPosition[0] <= attackEreaX)
-        && attackerPosition[1]-enemyPosition[1] <= attackEreaY
-        && attackerPosition[1]-enemyPosition[1] >= -attackEreaY) {
-        
-        if (attackerName === 'A' && turn === true) {
-            setEnemyHp(enemyHp - damage);
-            setTurn(!turn);
-        } else if (attackerName === 'B' && turn === false) {
-            setEnemyHp(enemyHp - damage);
-            setTurn(!turn);
-        }
-    }
-}
-
-
-
 function Game() {
     const [positionA, setPositionA] = useState([0, 4]);
     const [positionB, setPositionB] = useState([9, 4]);
     const [boardSize, setBoardSize] = useState([10, 9]);
     const [turn, setTurn] = useState(true);
+    const [firstTurn, setFirstTurn] = useState(true);
     const [step, setStep] = useState(0);
     const [hpA, setHpA] = useState(6);
     const [hpB, setHpB] = useState(6);
+    const [attackEreaA, setAttackEreaA] = useState([0, 0, 0]);
+    const [attackEreaB, setAttackEreaB] = useState([0, 0, 0]);
     const [defenceA, setDefenceA] = useState(false);
     const [defenceB, setDefenceB] = useState(false);
     const [winner, setWinner] = useState(null);
@@ -98,8 +160,10 @@ function Game() {
     useEffect(() => {
         if (turn === true) {
             setDefenceA(false);
+            setAttackEreaA([0, 0, 0]);
         } else {
             setDefenceB(false);
+            setAttackEreaB([0, 0, 0]);
         }
     }, [turn]);
 
@@ -150,7 +214,6 @@ function Game() {
         }
 
         if (event.key === 'Enter') {
-            attack(step, turn, positionA, positionB, setWinner, hpA, hpB, setHpA, setHpB);
             manageTurn(step, turn, setStep, setTurn);
         }
     };
@@ -161,19 +224,22 @@ function Game() {
                 <h1>{winner}</h1>
                 <p>{turn ? 'My' : 'Enemy\'s'} Turn</p>
                 <p>Player A's HP: {hpA}</p>
-                <button onClick={() => attack('A', positionA, positionB, 1, 1, setWinner, hpB, setHpB, turn, setTurn, 2)}>攻撃</button>
-                <button onClick={() => attack('A', positionA, positionB, 2, 1, setWinner, hpB, setHpB, turn, setTurn, 1)}>たま</button>
+                <button onClick={() => attack(turn, setTurn, setAttackEreaA, 'A', 1, 1, 2)}>攻撃</button>
+                <button onClick={() => attack(turn, setTurn, setAttackEreaA, 'A', 2, 1, 1)}>たま</button>
                 <button onClick={() => manageTurn(step, turn, setStep, setTurn)}>ようすみ</button>
             </div>
 
-            <Board boardSize={boardSize} positionA={positionA} positionB={positionB}/>
+            <Board boardSize={boardSize} positionA={positionA} positionB={positionB} 
+            attackEreaA={attackEreaA} attackEreaB={attackEreaB}
+            setAttackEreaA={setAttackEreaA} setAttackEreaB={setAttackEreaB}
+            hpA={hpA} setHpA={setHpA} hpB={hpB} setHpB={setHpB}/>
 
             <div className="state B">
                 <h1>{winner}</h1>
                 <p>{turn ? 'Enemy\'s' : 'My'} Turn</p>
                 <p>Player B's HP: {hpB}</p>
-                <button onClick={() => attack('B', positionB, positionA, 1, 1, setWinner, hpA, setHpA, turn, setTurn, 2)}>攻撃</button>
-                <button onClick={() => attack('B', positionB, positionA, 2, 1, setWinner, hpA, setHpA, turn, setTurn, 1)}>たま</button>
+                <button onClick={() => attack(turn, setTurn, setAttackEreaB, 'B', 1, 1, 2)}>攻撃</button>
+                <button onClick={() => attack(turn, setTurn, setAttackEreaB, 'B', 2, 1, 1)}>たま</button>
                 <button onClick={() => manageTurn(step, turn, setStep, setTurn)}>ようすみ</button>
             </div>
         </div>
